1346d9c77a57c74019a71ddc3eaf334730cad394
JeppeBondeBakkensen <jeppebondebakkensen@gmail.com>
2026-05-25 08:59:03 +0200

    Opgave 4 og 5

--- Opgave_1.cs
+++ Opgave_1.cs
@@ -1,16 +1,13 @@
-public static class Opgave_1
-{
+public static class Opgave_1 {
     // n = number of items in stream
-    public static IEnumerable<Tuple<ulong, int>> CreateStream(int n, int l)
-    {
+    public static IEnumerable<Tuple<ulong, int>> CreateStream(int n, int l) {
         // We generate a random uint64 number.
         Random rnd = new System.Random();
         ulong a = 0UL;
         byte[] b = new byte[8];
         rnd.NextBytes(b);
 
-        for (int i = 0; i < 8; ++i)
-        {
+        for (int i = 0; i < 8; ++i) {
             a = (a << 8) + (ulong)b[i] + 1;
         }
 
@@ -18,45 +15,38 @@ public static class Opgave_1
         a = (a | ((1UL << 31) - 1UL)) ^ ((1UL << 30) - 1UL);
         ulong x = 0UL;
 
-        for (int i = 0; i < n / 3; ++i)
-        {
+        for (int i = 0; i < n / 3; ++i) {
             x = x + a;
             yield return Tuple.Create(x & (((1UL << l) - 1UL) << 30), 1);
         }
 
-        for (int i = 0; i < (n + 1) / 3; ++i)
-        {
+        for (int i = 0; i < (n + 1) / 3; ++i) {
             x = x + a;
             yield return Tuple.Create(x & (((1UL << l) - 1UL) << 30), -1);
         }
 
-        for (int i = 0; i < (n + 2) / 3; ++i)
-        {
+        for (int i = 0; i < (n + 2) / 3; ++i) {
             x = x + a;
             yield return Tuple.Create(x & (((1UL << l) - 1UL) << 30), 1);
         }
     }
 
-    public static ulong Mod(ulong x, ulong p, int q)
-    {
+    public static ulong Mod(ulong x, ulong p, int q) {
         ulong y = (x & p) + (x >> q);
-        if (y >= p)
-        {
+        if (y >= p) {
             y -= p;
         }
 
         return y;
     }
 
-    public static ulong MultShift(ulong x)
-    {
+    public static ulong MultShift(ulong x) {
         ulong a = 102682031;
         int l = 3;
         return (a * x) >> (64 - l);
     }
 
-    public static ulong MultModPrime(ulong x)
-    {
+    public static ulong MultModPrime(ulong x) {
         int q = 89;
         ulong p = (1UL << q) - 1UL;
         ulong a = 102682031;
--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
 // Program.cs
-if (args.Length == 0)
-{
+if (args.Length == 0) {
     Console.WriteLine("Hvilken opgave skal testes?");
     Console.WriteLine("  Opgave 1: dotnet run 1");
     Console.WriteLine("  Opgave 1 speedtest: dotnet run 2");
     Console.WriteLine("  Opgave 2: dotnet run 3   ");
     Console.WriteLine("  Opgave 3: dotnet run 4");
+    Console.WriteLine("  Opgave 4: dotnet run 5");
     return;
 }
 
-switch (args[0])
-{
+switch (args[0]) {
     case "1":
         Opgave_1_Tests.TestMultShift();
         Opgave_1_Tests.TestMultModPrime();
@@ -29,6 +28,10 @@ switch (args[0])
         break;
     case "4":
         Opgave2_Tests.TestSquaresum();
+        break;
+    case "5":
+        Opgave_4_Tests.TestAlg1();
+        Opgave_4_Tests.TestBSC_init();
         break;
     default:
         Console.WriteLine("Må være en test mellem 1 - 4");
